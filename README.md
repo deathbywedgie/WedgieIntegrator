ecd295b0b04475f35d9da2ab2550d064beede663
Chad Roberts <jcbroberts@gmail.com>
2024-07-19 13:12:08 -0700

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WedgieIntegrator
-An API client toolkit that is async friendly
+WedgieIntegrator is an async friendly package for Python which acts as API client toolkit for creating and managing API clients with ease.
+
+## Features
+
+- Simple configuration
+- Multiple authentication strategies
+- Retry mechanisms
+- Asynchronous support
+- Detailed logging
+
+## Installation
+
+```bash
+pip install wedgieintegrator
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WedgieIntegrator",
+    version="0.1.0",
+    author="Chad Roberts",
+    author_email="jcbroberts@gmail.com",
+    description="An API client toolkit that is async friendly",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/deathbywedgie/WedgieIntegrator",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "httpx",
+        "pydantic",
+        "tenacity",
+        "structlog"
+    ],
+)
