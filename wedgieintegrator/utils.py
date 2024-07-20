20276d5535faf73fca45524683dec9870e6d111d
Chad Roberts <jcbroberts@gmail.com>
2024-07-19 19:19:16 -0700

    fixed logic... hopefully

--- wedgieintegrator/utils.py
+++ wedgieintegrator/utils.py
@@ -4,5 +4,6 @@ def with_retries(func):
     """Decorator to add retries to a function based on config"""
     def wrapper(self, *args, **kwargs):
         retry_decorator = retry(stop=stop_after_attempt(self.config.retry_attempts), wait=wait_exponential(min=1, max=10))
-        return retry_decorator(func)(self, *args, **kwargs)
+        decorated_func = retry_decorator(func)
+        return decorated_func(self, *args, **kwargs)
     return wrapper
