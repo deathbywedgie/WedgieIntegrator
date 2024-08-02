95d9a7b69e81d049789f7e7389613836b8eb58e2
Chad Roberts <jcbroberts@gmail.com>
2024-08-02 13:17:56 -0700

    draft for rate limiting that is hopefully reliable for Python 3.7

--- WedgieIntegrator/client.py
+++ WedgieIntegrator/client.py
@@ -2,6 +2,7 @@ from typing import Optional, Any, Type, Union, Dict, List
 import httpx
 from pydantic import BaseModel, ValidationError
 from tenacity import RetryError
+from aiolimiter import AsyncLimiter
 
 from .auth import AuthStrategy, NoAuth
 from .exceptions import RateLimitError, RateLimitFailure, TaskAborted
@@ -25,13 +26,14 @@ class APIClient:
     # retry_attempts: int = 3
     timeout: Optional[float] = 10.0  # Default timeout of 10 seconds
     verify_ssl: bool = True
-    # ToDo Future use
+    requests_per_second: int = None
     requests_per_minute: int = None
     verbose: bool = False
 
     auth_strategy: Optional[AuthStrategy] = None
     response_class: Optional[Type[APIResponse]] = None
     response_model: Optional[Type[BaseModel]] = None
+    limiter: Optional[AsyncLimiter] = None
 
     def __init__(self,
                  base_url: str,
@@ -42,18 +44,26 @@ class APIClient:
                  timeout: float = 10.0,
                  verify_ssl: bool = True,
                  requests_per_minute: int = None,
+                 requests_per_second: int = None,
                  verbose: bool = False,
                  ):
         self.base_url = base_url
         self.timeout = timeout
         self.verify_ssl = verify_ssl
         self.requests_per_minute = requests_per_minute
+        self.requests_per_second = requests_per_second
         self.verbose = verbose
         self.auth_strategy = auth_strategy or NoAuth()
         self.response_class = response_class or APIResponse
         self.response_model = response_model
         # Initialize client here
         self.client = httpx.AsyncClient(base_url=self.base_url, timeout=self.timeout, verify=self.verify_ssl)
+
+        # Initialize rate limiter
+        if self.requests_per_minute:
+            self.limiter = AsyncLimiter(self.requests_per_minute, time_period=60)
+        elif self.requests_per_second:
+            self.limiter = AsyncLimiter(self.requests_per_second, time_period=1)
 
     async def __aenter__(self) -> 'APIClient':
         # No need to initialize the client here as it is already initialized in __init__
@@ -75,6 +85,21 @@ class APIClient:
             await response_obj._async_parse_content()
         return response_obj
 
+    async def _perform_request(self, method: str, endpoint: str, **kwargs) -> httpx.Response:
+        """Helper function to send the HTTP request."""
+        request = self.client.build_request(method=method, url=endpoint, **kwargs)
+        self.auth_strategy.authenticate(request)
+        return await self.client.send(request)
+
+    async def _handle_response(self, response: httpx.Response, request: httpx.Request) -> APIResponse:
+        """Process the response and handle errors."""
+        response_obj = await self.create_response_object(response=response)
+        if response_obj.is_rate_limit_error is True:
+            raise RateLimitError("Rate limit error", request=request, response=response)
+        if response_obj.is_rate_limit_failure is True:
+            raise RateLimitFailure("Rate limit failure", request=request, response=response)
+        return response_obj
+
     @paginate_requests
     async def send_request(self, method: str, endpoint: str, raise_for_status=True, result_limit: int = None, **kwargs) -> Union[httpx.Response, APIResponse, Dict, List, Any]:
         """Send an HTTP request with retries and authentication"""
@@ -87,16 +112,14 @@ class APIClient:
         if self.client is None:
             raise RuntimeError("HTTP client is not initialized")
 
-        request = self.client.build_request(method=method, url=endpoint, **kwargs)
-        self.auth_strategy.authenticate(request)
         try:
-            response = await self.client.send(request)
+            if self.limiter:
+                async with self.limiter:
+                    response = await self._perform_request(method, endpoint, **kwargs)
+            else:
+                response = await self._perform_request(method, endpoint, **kwargs)
             self.log_verbose("Received response", status_code=response.status_code, logger=__logger)
-            response_obj = await self.create_response_object(response=response)
-            if response_obj.is_rate_limit_error is True:
-                raise RateLimitError("Rate limit error", request=request, response=response)
-            if response_obj.is_rate_limit_failure is True:
-                raise RateLimitFailure("Rate limit failure", request=request, response=response)
+            response_obj = await self._handle_response(response, response.request)
             if raise_for_status:
                 response.raise_for_status()
         except httpx.HTTPStatusError as e:
