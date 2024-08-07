e6a9873732036cd9af7e5907d9780274c39b5bf9
Chad Roberts <jcbroberts@gmail.com>
2024-08-06 19:15:48 -0700

    Fix: Better fix fo asyncio.run. Previous one worked once but then failed thereafter

--- WedgieIntegrator/asyncio_workaround.py
+++ WedgieIntegrator/asyncio_workaround.py
@@ -1,19 +1,30 @@
-# Workaround for Python 3.7 because asyncio.run and asyncio.to_thread were added in 3.8
+# Workarounds for asyncio in Python 3.7
 
-from asyncio import get_event_loop
+import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 executor = ThreadPoolExecutor()
 
 
-async def to_thread(func, *args, **kwargs):
-    loop = get_event_loop()
-    return await loop.run_in_executor(executor, func, *args, **kwargs)
-
-
 # Workaround for Python 3.7 because asyncio.run was added in 3.8
 def run(coro):
-    loop = get_event_loop()
-    result = loop.run_until_complete(coro)
-    loop.close()
-    return result
+    try:
+        loop = asyncio.get_event_loop()
+        if loop.is_closed():
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    try:
+        return loop.run_until_complete(coro)
+    finally:
+        if not loop.is_running():
+            loop.close()
+
+
+# Workaround for Python 3.7 because asyncio.run was added in 3.9
+async def to_thread(func, *args, **kwargs):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(executor, func, *args, **kwargs)
