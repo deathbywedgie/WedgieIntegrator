c7e6a332ef06b67ec39aeafee0cc9d5ae32ea8f9
Chad Roberts <jcbroberts@gmail.com>
2024-08-09 18:59:35 -0700

    save the client as a private but not hidden object, or custom exceptions can't access it

--- WedgieIntegrator/response.py
+++ WedgieIntegrator/response.py
@@ -18,10 +18,10 @@ class BaseAPIResponse:
     link_header: str = None
     pagination_links: dict = None
     __content = None
-    __client = None
+    _client = None
 
     def __init__(self, api_client, response: httpx.Response, response_model: Optional[Type[BaseModel]] = None):
-        self.__client = api_client
+        self._client = api_client
         self.response = response
         self.response_model = response_model
         self.content_type = response.headers.get('Content-Type', '')
