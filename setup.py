ce67fa1f7bbc234207034c7b1531775f4288bfe6
Chad Roberts <jcbroberts@gmail.com>
2024-08-11 13:30:09 -0700

    narrowing down the scope for folders to include in packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/deathbywedgie/WedgieIntegrator",
-    packages=find_packages(),
+    packages=find_packages(include=['WedgieIntegrator', 'WedgieIntegrator.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
