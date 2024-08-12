f17f2648c12e224642995782e52174e325ba40a5
Chad Roberts <jcbroberts@gmail.com>
2024-08-12 11:24:54 -0700

    accept result_limit as an input in order to know about the original call

--- WedgieIntegrator/response.py
+++ WedgieIntegrator/response.py
@@ -18,12 +18,14 @@ class BaseAPIResponse:
     pagination_links: dict = None
     __content = None
     _client = None
+    result_limit: int = None
 
-    def __init__(self, api_client, response: httpx.Response, response_model: Optional[Type[BaseModel]] = None):
+    def __init__(self, api_client, response: httpx.Response, response_model: Optional[Type[BaseModel]] = None, result_limit: int = None):
         self._client = api_client
         self.response = response
         self.response_model = response_model
         self.content_type = response.headers.get('Content-Type', '')
+        self.result_limit = result_limit
 
     @property
     def is_rate_limit_error(self):
